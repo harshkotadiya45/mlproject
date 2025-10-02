6f6d238a8f6820817e25727a8cd759ae6f0bd643
harshkotadiya45 <23dcs051@charusat.edu.in>
2025-10-02 16:07:13 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ HYPHEN_E_DOT = "-e ."
 def get_requirements(file_path:str)->List[str] :
     requirments = []
     with open(file_path) as file_obj:
-        requirments = file_obj.readline()
-        requirments = [req.replace("\n", " ") for req in requirments]
+        requirments = file_obj.readlines()
+        requirments = [req.replace("\n", "") for req in requirments]
 
         if HYPHEN_E_DOT in requirments:
             requirments.remove(HYPHEN_E_DOT)
@@ -18,5 +18,5 @@ setup(
     author="Harsh",
     author_email="23dcs051@charusat.edu.in",
     packages=find_packages(),
-    install_requirements = get_requirements("requirements  .txt")
+    install_requirements = get_requirements("requirements.txt")
 )
