12d36953c0d31e99c8b59bb127c56ce6fee5e923
harshkotadiya45 <23dcs051@charusat.edu.in>
2025-10-05 10:45:51 +0530

    modified

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -74,7 +74,6 @@ class DataTransformation:
             raise CustomException(e,sys)
         
     def initiate_data_transformation(self,train_path,test_path):
-
         try:
             train_df=pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
